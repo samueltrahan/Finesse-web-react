eb85362719b0cabd0cfd806932893c293fca59b3
Samuel Trahan <samtrahan91@gmail.com>
2020-11-04 19:51:51 -0600

    still need to do alot more styling on this navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,10 +5,15 @@ export default function NavBar() {
   return (
     <div className="navbar">
       <nav>
-        <div className="about-home">
+        <div className="left-navbar">
         <a className="links" href="/">Home</a>
         <a className="links" href="/about">About</a>
         <a className="links" href="/started">Getting Started</a>
+        </div>
+        <div className="right-navbar">
+          <a className="socials" href="https://github.com/samueltrahan/Finesse-web-react"><i className="fab fa-github fa-2x"></i></a>
+          <a className="socials" href="https://twitter.com/SamuelTrahan4"><i className="fab fa-twitter fa-2x"></i></a>
+          <a className="subscribe" href="/subscribe">Subcribe</a>
         </div>
       </nav>
     </div>
