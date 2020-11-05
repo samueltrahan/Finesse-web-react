5bd5c6bc47add511ffaccdf74b5cc5ffa5d99480
Samuel Trahan <samtrahan91@gmail.com>
2020-11-05 14:52:37 -0600

    source section complete

--- src/components/Source/Source.js
+++ src/components/Source/Source.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import './Source.css';
+import React from "react";
+import "./Source.css";
+import Pic01 from "../../assets/source.png";
+import Pic02 from '../../assets/finesse-single-logo.png'
 
 export default function Source() {
   return (
-    <div>
-      Source Section
-    </div>
-  )
+      <section className="source">
+        <div className="source-section-1">
+        <img className="source-img" alt="" src={Pic01}></img>
+        </div>
+        <div className="source-section-2">
+        <a className="source-link" href="/source">
+          Source
+          <br /> <h1>GitHub Source</h1>
+          <p>Where you can find this project on GitHub</p>
+        </a>
+        <br />
+        <img className="source-mini-logo"alt="" src={Pic02}></img>
+        </div>
+      </section>
+  );
 }
