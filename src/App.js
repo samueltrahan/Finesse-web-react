1ab0f54bb9c45a139d532a72f1a9d329605898ae
Samuel Trahan <samtrahan91@gmail.com>
2020-11-05 14:04:31 -0600

    source component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import Banner from './components/Banner/Banner';
+import Source from './components/Source/Source';
 
 export default function App() {
   return (
     <div>
       <NavBar />
       <Banner />
+      <Source />
     </div>
   )
 }
---
+++ src/components/Source/Source.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Source.css';
+
+export default function Source() {
+  return (
+    <div>
+      Source Section
+    </div>
+  )
+}
