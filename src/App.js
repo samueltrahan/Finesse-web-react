566a34d8dab299e9b21c99b7b538f9115835da2f
Samuel Trahan <samtrahan91@gmail.com>
2020-11-06 11:14:09 -0600

    footer section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NavBar from "./components/NavBar/NavBar";
 import Banner from "./components/Banner/Banner";
 import Source from "./components/Source/Source";
 import Blog from "./components/Blog/Blog";
+import Footer from './components/Footer/Footer';
 import Pic04 from "./assets/temper.jpg";
 import Pic05 from './assets/medium.png';
 import Pic06 from './assets/website.jpg';
@@ -14,12 +15,15 @@ export default function App() {
     <Router>
       <NavBar />
       <Banner />
+      <div className="source-section">
       <Source />
+      </div>
       <div className="blog-section">
         <Blog path="/welcome" image={Pic04} head='Finesse' title='Welcome to Finesse' paragraph="This article will help you get started with transforming this template into your own beautiful website."/>
         <Blog path="/process" image={Pic05} head='Medium' title="The Process" paragraph="This article is a Medium article on the process of build this template. There is some helpful hints on styling and customization in the article." />
         <Blog path="/inspiration" image={Pic06} head="Inspiration" title="The Purpose of a Website" paragraph="This article will show you why you need a website and even if you don't have a business, having a website is critical for anything you want to do."/>
       </div>
+      <Footer />
     </Router>
   );
 }
