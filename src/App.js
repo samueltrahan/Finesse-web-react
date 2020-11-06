88bf864c2c5b6fbd2af28aaa325c495aac9235b4
Samuel Trahan <samtrahan91@gmail.com>
2020-11-06 09:55:28 -0600

    blog section styled and content

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-import './App.css';
-import NavBar from './components/NavBar/NavBar';
-import Banner from './components/Banner/Banner';
-import Source from './components/Source/Source';
-import Blog from './components/Blog/Blog';
-import Pic04 from './assets/temper.jpg';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+import Banner from "./components/Banner/Banner";
+import Source from "./components/Source/Source";
+import Blog from "./components/Blog/Blog";
+import Pic04 from "./assets/temper.jpg";
+import Pic05 from './assets/medium.png';
+import Pic06 from './assets/website.jpg';
 
 export default function App() {
   return (
@@ -14,8 +16,10 @@ export default function App() {
       <Banner />
       <Source />
       <div className="blog-section">
-      <Blog image={Pic04}/>
+        <Blog image={Pic04} head='Finesse' title='Welcome to Finesse' paragraph="This article will help you get started with transforming this template into your own beautiful website."/>
+        <Blog image={Pic05} head='Medium' title="The Process" paragraph="This article is a Medium article on the process of build this template. There is some helpful hints on styling and customization in the article." />
+        <Blog image={Pic06} head="Inspiration" title="The Purpose of a Website" paragraph="This article will show you why you need a website and even if you don't have a business, having a website is critical for anything you want to do."/>
       </div>
     </Router>
-  )
+  );
 }
--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Blog.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Blog.css";
 
-export default function Blog({image}) {
+export default function Blog({ image, head, title, paragraph }) {
   return (
     <>
-    <hr className="solid"></hr>
-    <Link to="/blog">
-      <div className="cards">
-        <div className="image">
-          <img alt="" src={image}></img>
+      <Link to="/blog">
+        <div className="cards">
+          <div className="image">
+            <img alt="" src={image} style={{ width: '250px', height: '180px', borderRadius: '10px 10px 0 0'}}></img>
+          </div>
+          <div className="elements">
+            <h5 className="head">{head}</h5>
+            <h3 className="title">{title}</h3>
+            <p className="paragraph">{paragraph}</p>
+          </div>
         </div>
-        <div>
-          <h5>tests</h5>
-          <h3>The Title</h3>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa obcaecati consequatur error blanditiis quod maiores odio vel aspernatur neque voluptas, eius deserunt beatae veniam est, nihil aut! Error, quae quibusdam.</p>
-        </div>
-      </div>
-    </Link>
+      </Link>
     </>
-  )
+  );
 }
