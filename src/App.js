8f060bab7214095daa011b384444812c31b43691
Samuel Trahan <samtrahan91@gmail.com>
2020-11-05 19:44:30 -0600

    blog section styling and componet set up

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import Banner from './components/Banner/Banner';
 import Source from './components/Source/Source';
 import Blog from './components/Blog/Blog';
+import Pic04 from './assets/temper.jpg';
 
 export default function App() {
   return (
-    <div>
+    <Router>
       <NavBar />
       <Banner />
       <Source />
-      <Blog />
-    </div>
+      <div className="blog-section">
+      <Blog image={Pic04}/>
+      </div>
+    </Router>
   )
 }
--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Blog.css';
 
-export default function Blog() {
+export default function Blog({image}) {
   return (
-    <div>
-      Blog section
-    </div>
+    <>
+    <hr className="solid"></hr>
+    <Link to="/blog">
+      <div className="cards">
+        <div className="image">
+          <img alt="" src={image}></img>
+        </div>
+        <div>
+          <h5>tests</h5>
+          <h3>The Title</h3>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa obcaecati consequatur error blanditiis quod maiores odio vel aspernatur neque voluptas, eius deserunt beatae veniam est, nihil aut! Error, quae quibusdam.</p>
+        </div>
+      </div>
+    </Link>
+    </>
   )
 }
