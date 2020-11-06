808feb7d2dfe92aa62e7b4fbb077699c08bfaa6d
Samuel Trahan <samtrahan91@gmail.com>
2020-11-06 11:17:10 -0600

    fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
 import Banner from "./components/Banner/Banner";
